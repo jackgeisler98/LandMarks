df1cfb92258373a002f970c89f1b8f7ecc283424
Jack Geisler <jackgeisler98@gmail.com>
2024-04-11 15:03:19 -0700

    End of first part of tutorial

---
+++ LandMarks/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  LandMarks
+//
+//  Created by Jack Geisler on 4/11/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
